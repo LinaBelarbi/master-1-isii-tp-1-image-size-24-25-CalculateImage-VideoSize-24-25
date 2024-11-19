47c599ed1914e7815655515af605ffa443a65e80
LinaBelarbi <belarbi2023lina@gmail.com>
2024-11-19 11:02:58 +0100

    Update grayScaleImage.c

--- grayScaleImage.c
+++ grayScaleImage.c
@@ -6,7 +6,7 @@
 //   grayScale size of the image Bitmap (in byte)
 float grayScaleImage(int w, int h) {
    // YOUR CODE HERE - BEGIN
-
+ return w * h;  // La taille en octets de l'image en niveaux de gris
    // YOUR CODE HERE - END
    return 0;
 }
